0304a16f7ef9c03727f09578547bcd73ecc01372
Corey Lin <corey@Pros-MacBook-Pro.local>
2015-10-04 04:20:29 -0700

    two pane layout

--- app/src/main/java/com/kivalocalteam10/kivalocal/MainActivity.java
+++ app/src/main/java/com/kivalocalteam10/kivalocal/MainActivity.java
@@ -11,48 +11,109 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseQueryAdapter;
 
 import java.util.List;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
 
 public class MainActivity extends ActionBarActivity {
 
     List<ParseObject> mBusinessData;
+    ViewGroup listContent;
+    ViewGroup mapContent;
+    ContentType contentType = ContentType.LIST;
+    enum ContentType { LIST, MAP };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        searchNearbyPlaces("");
+        final EditText text = (EditText)findViewById(R.id.search_edit_text);
+        listContent = (ViewGroup)findViewById(R.id.result_list_view);
+        mapContent = (ViewGroup)findViewById(R.id.result_map_view);
+
+        updateContentView(createQuery(""));
 
         Button b = (Button)findViewById(R.id.search_button);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 EditText text = (EditText)findViewById(R.id.search_edit_text);
-                searchNearbyPlaces(text.getText().toString());
+                updateContentView(createQuery(text.getText().toString()));
+            }
+        });
+
+        b = (Button)findViewById(R.id.list_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Switch to List View", LENGTH_SHORT).show();
+                if(contentType != ContentType.LIST) {
+                    contentType = ContentType.LIST;
+                    updateContentView(createQuery(text.getText().toString()));
+                }
+            }
+        });
+
+        b = (Button)findViewById(R.id.map_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Switch to Map View", LENGTH_SHORT).show();
+                if(contentType != ContentType.MAP) {
+                    contentType = ContentType.MAP;
+                    updateContentView(createQuery(text.getText().toString()));
+                }
             }
         });
     }
 
-    private void searchNearbyPlaces(final String keyword) {
+    private ParseQuery createQuery(final String keyword) {
+        ParseQuery query = new ParseQuery("TestObject");
+        query.whereContains("Name", keyword);
+        return query;
+    }
+
+    private void updateContentView(ParseQuery query) {
+        if(contentType == ContentType.LIST) {
+            createListViewAdapter(query);
+            listContent.setVisibility(View.VISIBLE);
+            mapContent.setVisibility(View.GONE);
+        } else {
+            query.findInBackground(new FindCallback() {
+                @Override
+                public void done(Object objects, Throwable t) {}
+                @Override
+                public void done(List objects, ParseException e) {
+                    //TODO: add map code here
+                }
+            });
+            listContent.setVisibility(View.GONE);
+            mapContent.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void createListViewAdapter(final ParseQuery query) {
         // Initialize a ParseQueryAdapter
         BusinessAdapter adapter = new BusinessAdapter(this,
                 new ParseQueryAdapter.QueryFactory<ParseObject>() {
                     public ParseQuery<ParseObject> create() {
                         // Here we can configure a ParseQuery to our heart's desire.
-                        ParseQuery query = new ParseQuery("TestObject");
-                        query.whereContains("Name", keyword);
                         return query;
                     }
                 });
-// Custom ParseQueryAdapter, for all ParseQueryAdapter setting please check our doc
+        // Custom ParseQueryAdapter, for all ParseQueryAdapter setting please check our doc
         adapter.setTextKey("Name");
+
         //adapter.addOnQueryLoadListener();
         //adapter.setImageKey("photo");
         ListView listView = (ListView) findViewById(R.id.result_list_view);
